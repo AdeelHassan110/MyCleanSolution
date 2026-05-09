01c799240ec9c6aa5d5903feb0b6f0388bba063e
AdeelHassan110 <adeellagharibaloch@gmail.com>
2026-05-09 14:40:31 +0500

    main branch: added loging

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddSwaggerGen();
 
 // Register Repository
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+Console.WriteLine("Repository registered - Main branch fix");
 
 // ? Register MediatR (Application layer ke handlers ke liye)
 builder.Services.AddMediatR(typeof(Application.DTOs.StudentDto).Assembly);
